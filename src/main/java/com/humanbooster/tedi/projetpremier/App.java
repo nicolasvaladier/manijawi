a8ae97326f62c5b6c688207ddeab23b0173b9cbc
William Olliso <will.olliso2@gmail.com>
2015-03-24 16:30:35 +0100

    Automatic changes linked to my plugins I think

--- src/main/java/com/humanbooster/tedi/projetpremier/App.java
+++ src/main/java/com/humanbooster/tedi/projetpremier/App.java
@@ -4,10 +4,9 @@ package com.humanbooster.tedi.projetpremier;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Goodbye cruel world!");
+	}
 }
