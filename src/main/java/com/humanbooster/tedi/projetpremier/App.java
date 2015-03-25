22c961e416377573fa183b960f86e96dcc04b806
William Olliso <will.olliso2@gmail.com>
2015-03-25 11:08:12 +0100

    To be or not to be. Line 10 app.java

--- src/main/java/com/humanbooster/tedi/projetpremier/App.java
+++ src/main/java/com/humanbooster/tedi/projetpremier/App.java
@@ -7,6 +7,6 @@ package com.humanbooster.tedi.projetpremier;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Goodbye cruel world!!");
+		System.out.println("To be, or not to be? That is the question.");
 	}
 }
