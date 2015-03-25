3d3a606140a7c44eca1d2cf94ce68da88018e973
Nicolas Valadier <nicolas.valadier@gmail.com>
2015-03-25 09:12:23 +0100

    changement de branche

--- src/main/java/com/humanbooster/tedi/projetpremier/App.java
+++ src/main/java/com/humanbooster/tedi/projetpremier/App.java
@@ -4,10 +4,9 @@ package com.humanbooster.tedi.projetpremier;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("travail sur ma branche de travail");
+	}
 }
