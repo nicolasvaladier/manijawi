8c6d18fede42a522b55ae912a88886d8daba0422
grmt <gerente.mt@gmx.fr>
2015-03-25 13:21:34 +0100

    ajout methode sur FichierEleve

--- src/test/java/com/humanbooster/tedi/projetpremier/FicheEleve.java
+++ src/test/java/com/humanbooster/tedi/projetpremier/FicheEleve.java
@@ -1,7 +1,27 @@
 package com.humanbooster.tedi.projetpremier;
 
 public class FicheEleve {
-	private String nomeleve;
-	private String prenomeleve;
+	private String nomeleve = "";
+	private String prenomeleve = "";
 
+	public String getNomeleve() {
+		return nomeleve;
+	}
+
+	public String getPrenomeleve() {
+		return prenomeleve;
+	}
+
+	public void setNomeleve(final String nomeleve) {
+		this.nomeleve = nomeleve;
+	}
+
+	public void setPrenomeleve(final String prenomeleve) {
+		this.prenomeleve = prenomeleve;
+	}
+
+	public void FicheEleve(final String nomeleve, final String prenomeleve) {
+		this.nomeleve = nomeleve;
+		this.prenomeleve = prenomeleve;
+	}
 }
